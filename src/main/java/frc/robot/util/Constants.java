25591a2c9bb3d656c7d4aee22bd8d1911b22d0e9
6823Code <usmrobotics@ga.usmk12.org>
2023-01-20 16:09:51 -0600

    Updated constants

--- src/main/java/frc/robot/util/Constants.java
+++ src/main/java/frc/robot/util/Constants.java
@@ -3,8 +3,8 @@ package frc.robot.util;
 public class Constants {
     public static final double degToRad = Math.PI / 180;
     public static final double radToDeg = 180 / Math.PI;
-    public static final double fLOffset = 171.474609375;
-    public static final double fROffset = 81.298828125;
-    public static final double bLOffset = 301.728515625;
-    public static final double bROffset = 165.76171875;
+    public static final double fLOffset = 83.05573403835297;
+    public static final double fROffset = 257.6042193174362;
+    public static final double bLOffset = 164.79344800114632;
+    public static final double bROffset = 6.850845962762833;
 }
